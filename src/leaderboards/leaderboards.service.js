b9f78986a25c2c7b75d08a516089f2ce646e6c6e
FoolyTD <trcariaga@utexas.edu>
2021-07-14 08:48:22 -0500

    updated vercel.json

--- src/leaderboards/leaderboards.service.js
+++ src/leaderboards/leaderboards.service.js
@@ -3,12 +3,7 @@ const knex = require("../db/connection");
 function list() {
     return knex("leaderboards").select("*")
 }
-function create(entry) {
-    return knex("leaderboards")
-    .insert(entry);
-}
 
 module.exports = {
     list,
-    create,
 }
