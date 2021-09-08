c7c76f84c014d4004e6323f152ebacd771fb5d1a
FoolyTD <trcariaga@utexas.edu>
2021-09-08 15:38:35 -0500

    updated middleware pipeline

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const methodNotAllowed = require("../errors/methodNotAllowed");
 const controller = require("./users.controller");
 
+router.route("/").post(controller.create).all(methodNotAllowed);
+router.route("/all").post(controller.list).all(methodNotAllowed);
+router.route("/login").post(controller.login).all(methodNotAllowed);
 router
   .route("/:userId")
   .get(controller.read)
   .post(controller.update)
   .delete(controller.delete)
   .all(methodNotAllowed);
-router.route("/").post(controller.create).all(methodNotAllowed);
-router.route("/all").post(controller.list).all(methodNotAllowed);
-router.route("/login").post(controller.login).all(methodNotAllowed);
 
 module.exports = router;
